1f73f8a37b7fef25165742f3143641dd4f76da8e
gobne <tomcos61@libero.it>
2017-09-04 17:36:33 +0200

    Resolved title bug

---
+++ languages/ts/italian.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>statisticswindow</name>
+    <message>
+        <source>K</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Y</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>settingswindow</name>
+    <message>
+        <source>Info</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Salva</translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Fatal</source>
+        <translation>Fatale</translation>
+    </message>
+    <message>
+        <source>Italian</source>
+        <translation>Italiano</translation>
+    </message>
+    <message>
+        <source>Choose download path</source>
+        <translation>Scegli percorso di download</translation>
+    </message>
+    <message>
+        <source>Click to select download path for torrents</source>
+        <translation>Clicca per selezionare il percorso di  download dei torrent</translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation>Cancella</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Critical</source>
+        <translation>Critico</translation>
+    </message>
+    <message>
+        <source>Debug Level</source>
+        <translation>Livello di Debug</translation>
+    </message>
+    <message>
+        <source>Download path</source>
+        <translation>Percorso di download</translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation>Inglese</translation>
+    </message>
+    <message>
+        <source>Select Language (reboot required)</source>
+        <translation>Seleziona Lingua (riavvio richiesto)</translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>itemDetails</name>
+    <message>
+        <source>Name</source>
+        <translation>Nome</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Dimensione</translation>
+    </message>
+    <message>
+        <source>Pieces Count</source>
+        <translation>Numero Chunk</translation>
+    </message>
+    <message>
+        <source>Torrent info</source>
+        <translation>Torrent Info</translation>
+    </message>
+    <message>
+        <source>Pieces Length</source>
+        <translation>Lunghezza Chunk</translation>
+    </message>
+    <message>
+        <source>Sequential download</source>
+        <translation>Download Sequenziale</translation>
+    </message>
+    <message>
+        <source>Priority</source>
+        <translation>Priorità</translation>
+    </message>
+    <message>
+        <source>Progress</source>
+        <translation>Progresso</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>User</source>
+        <translation>Utente</translation>
+    </message>
+    <message>
+        <source>Torrents (*.torrent)</source>
+        <translation>Torrents</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Cerca</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Download</translation>
+    </message>
+    <message>
+        <source>Select torrent</source>
+        <translation>Seleziona Torrent</translation>
+    </message>
+    <message>
+        <source>Archive</source>
+        <translation>Archivio</translation>
+    </message>
+    <message>
+        <source>Streaming</source>
+        <translation>Streaming</translation>
+    </message>
+    <message>
+        <source>Statistics</source>
+        <translation>Statistiche</translation>
+    </message>
+    <message>
+        <source>Add Torrent</source>
+        <translation>Aggiungi Torrent</translation>
+    </message>
+    <message>
+        <source>Turn off Tsuanmi</source>
+        <translation>Spegni Tsunami</translation>
+    </message>
+    <message>
+        <source>Open Chat</source>
+        <translation>Apri Chat</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+</context>
+<context>
+    <name>tsuItem</name>
+    <message>
+        <source>Total</source>
+        <translation>Totale</translation>
+    </message>
+    <message>
+        <source>Remaining</source>
+        <translation>Rimanente</translation>
+    </message>
+    <message>
+        <source>Downloaded</source>
+        <translation>Scaricato</translation>
+    </message>
+</context>
+</TS>
