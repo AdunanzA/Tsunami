1603d0308342e53f19237952c8a35813edacadb6
rickyi89 <riccardo.ierardi@gmail.com>
2017-12-05 23:28:49 +0100

    Streaming Update
    - Added support for full screen in Streaming window (double-click on video and dedicated button)
    - Reviewed Streming interface
    - Added autopause and quick rewind when changing window during Streaming

--- previewplayerwindow.cpp
+++ previewplayerwindow.cpp
@@ -1,9 +1,13 @@
 #include "previewplayerwindow.h"
 #include "ui_previewplayerwindow.h"
 #include <QFileDialog>
+#include <QKeyEvent>
 
 #define PLAY_STRING "Play"
 #define PAUSE_STRING "Pause"
+
+#define PLAY_IMAGE_RESOURCE ":/images/tsuplayer/Play.png"
+#define PAUSE_IMAGE_RESOURCE ":/images/tsuplayer/Pause.png"
 
 PreviewPlayerWindow::PreviewPlayerWindow(QWidget *parent) :
     QWidget(parent),
@@ -11,11 +15,11 @@ PreviewPlayerWindow::PreviewPlayerWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-
     previewPlayer = new PreviewPlayer(ui->videoAreaWidget);
     ui->videoAreaWidget->setMediaPlayer(previewPlayer->mediaPlayer);
     ui->volumeWidget->setMediaPlayer(previewPlayer->mediaPlayer);
     ui->volumeWidget->setVolume(50);
+    ui->volumeWidget->setOrientation(Qt::Orientation::Horizontal);              //Set audio slider horizontal
     //ui->volumeWidget->setTracking(true);
     ui->seekWidget->setMediaPlayer(previewPlayer->mediaPlayer);
     ui->seekWidget->setStyleSheet("color:white");                               //allows the time to be shown on a black background
@@ -27,6 +31,27 @@ PreviewPlayerWindow::PreviewPlayerWindow(QWidget *parent) :
 
     ui->pushButtonOpenFile->setStyleSheet("background-color:white");
     ui->pushButtonPlayPause->setStyleSheet("background-color:white");
+
+    QIcon tempIcon;
+    tempIcon.addFile(":/images/tsuplayer/Full_Screen.png", QSize(), QIcon::Normal, QIcon::Off);
+    ui->pushButtonFullscreen->setIcon(tempIcon);
+    ui->pushButtonFullscreen->setIconSize(QSize(30,30));
+    ui->pushButtonFullscreen->setText("");
+
+    tempIcon.addFile(":/images/tsuplayer/OpenFile.png", QSize(), QIcon::Normal, QIcon::Off);
+    ui->pushButtonOpenFile->setIcon(tempIcon);
+    ui->pushButtonOpenFile->setIconSize(QSize(30,30));
+    ui->pushButtonOpenFile->setText("");
+
+    updatePlayPauseButton();
+
+    QSlider *seekSlider = new QSlider(Qt::Orientation::Horizontal);
+    seekSlider->setStyleSheet("QSlider::groove:horizontal { background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #6B6A68, stop: 0.5 #212121, stop: 1 #000000); height: 8px; border-top-right-radius: 4px; border-bottom-right-radius: 4px; border-top-left-radius: 4px; border-bottom-left-radius: 4px; } QSlider::handle:horizontal {  } QSlider::add-page:horizontal { } QSlider::sub-page:horizontal { background: qlineargradient(x1: 0.2, y1: 0, x2: 0.2, y2: 1, stop: 0 #6BEAFD, stop: 0.5 #125292, stop: 1 #0F1014); border-top-right-radius: 4px; border-bottom-right-radius: 4px; border-top-left-radius: 4px; border-bottom-left-radius: 4px; margin: 0px 2px 0px 1px; }");
+    ui->seekWidget->setSliderWidget(seekSlider, true);
+
+    ui->volumeWidget->setStyleSheet("QSlider::groove:horizontal { background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #6B6A68, stop: 0.5 #212121, stop: 1 #000000); height: 8px; border-top-right-radius: 4px; border-bottom-right-radius: 4px; border-top-left-radius: 4px; border-bottom-left-radius: 4px; } QSlider::handle:horizontal {  } QSlider::add-page:horizontal { } QSlider::sub-page:horizontal { background: qlineargradient(x1: 0.2, y1: 0, x2: 0.2, y2: 1, stop: 0 #C2934D, stop: 0.5 #A07028, stop: 1 #895D1C); border-top-right-radius: 4px; border-bottom-right-radius: 4px; border-top-left-radius: 4px; border-bottom-left-radius: 4px; margin: 0px 2px 0px 1px; }");
+
+    qApp->installEventFilter(this);
     //this->setStyleSheet("background-color:white");
 }
 
@@ -54,14 +79,19 @@ void PreviewPlayerWindow::on_pushButtonPlayPause_clicked()
 
 void PreviewPlayerWindow::updatePlayPauseButton()
 {
+    QIcon playPauseIcon;
     if(previewPlayer->isPlaying)
     {
-        ui->pushButtonPlayPause->setText(PAUSE_STRING);
+        playPauseIcon.addFile(PAUSE_IMAGE_RESOURCE, QSize(), QIcon::Normal, QIcon::Off);
+
     }
     else
     {
-        ui->pushButtonPlayPause->setText(PLAY_STRING);
+        playPauseIcon.addFile(PLAY_IMAGE_RESOURCE, QSize(), QIcon::Normal, QIcon::Off);
     }
+    ui->pushButtonPlayPause->setIcon(playPauseIcon);
+    ui->pushButtonPlayPause->setIconSize(QSize(30,30));
+    ui->pushButtonPlayPause->setText("");
 }
 
 void PreviewPlayerWindow::mediaStopped()
@@ -69,4 +99,59 @@ void PreviewPlayerWindow::mediaStopped()
     previewPlayer->isPlaying = false;
     updatePlayPauseButton();
     previewPlayer->ResetCurrentMedia();
+}
+
+void PreviewPlayerWindow::PauseForHidden()
+{
+    if(previewPlayer->isPlaying)
+    {
+        on_pushButtonPlayPause_clicked();
+        previewPlayer->rewindForHidden();
+    }
+}
+
+void PreviewPlayerWindow::on_pushButtonFullscreen_clicked()
+{
+    //ui->videoAreaWidget->showFullScreen();
+    ui->videoAreaWidget->setWindowFlags(Qt::Window);
+    ui->videoAreaWidget->setWindowState(Qt::WindowFullScreen);
+    ui->videoAreaWidget->show();
+}
+
+bool PreviewPlayerWindow::eventFilter(QObject *obj, QEvent *event)
+{
+    QKeyEvent *keyEvent;
+    QEvent::Type actualEvent = event->type();
+    if (actualEvent == QEvent::KeyPress)
+    {
+        keyEvent = static_cast<QKeyEvent *>(event);
+    }
+    switch(actualEvent)
+    {
+        case QEvent::KeyPress:
+            if(keyEvent->key() == Qt::Key_Escape && obj == ui->videoAreaWidget)
+            {
+                if(ui->videoAreaWidget->isFullScreen())
+                {
+                   ui->videoAreaWidget->setWindowFlags(Qt::Widget);
+                   ui->videoAreaWidget->showNormal();
+                }
+            }
+        break;
+        case QEvent::MouseButtonDblClick:
+            if(ui->videoAreaWidget->underMouse())
+            {
+                if(ui->videoAreaWidget->isFullScreen())
+                {
+                    ui->videoAreaWidget->setWindowFlags(Qt::Widget);
+                    ui->videoAreaWidget->showNormal();
+                }
+                else
+                {
+                    on_pushButtonFullscreen_clicked();
+                }
+            }
+        break;
+    }
+    return QObject::eventFilter(obj, event);
 }
